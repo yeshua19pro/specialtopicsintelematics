bfd5e844d0b198796d03595c28025d3d96142c29
JoseDanielCU <jdcastaneu@eafit.edu.co>
2025-09-14 13:48:57 -0500

    integrar gRPC y protobuf en Worker

---
+++ Worker/build/CMakeFiles/worker.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worker.dir/gridmr.grpc.pb.cc.o"
+  "CMakeFiles/worker.dir/gridmr.grpc.pb.cc.o.d"
+  "CMakeFiles/worker.dir/gridmr.pb.cc.o"
+  "CMakeFiles/worker.dir/gridmr.pb.cc.o.d"
+  "CMakeFiles/worker.dir/worker.cpp.o"
+  "CMakeFiles/worker.dir/worker.cpp.o.d"
+  "worker"
+  "worker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
